80689bc4680594e96e298989869986d2ba2f2eff
akams <e.ndjawe@gmail.com>
2020-10-25 15:29:23 +0100

    [feat]: update validate function

--- src/containers/RegisterProspect/validate.js
+++ src/containers/RegisterProspect/validate.js
@@ -1,19 +1,21 @@
 export const compileValidation = (values) => {
   const errors = {};
   if (!values.company || values.company.trim() === '') {
-    errors.company = 'Required';
+    errors.company = 'Champ obligatoire';
   }
   if (!values.lastname || values.lastname.trim() === '') {
-    errors.lastname = 'Required';
+    errors.lastname = 'Champ obligatoire';
   }
   if (!values.firstname || values.firstname.trim() === '') {
-    errors.firstname = 'Required';
+    errors.firstname = 'Champ obligatoire';
   }
   if (!values.address || values.address.trim() === '') {
-    errors.address = 'Required';
+    errors.address = 'Champ obligatoire';
   }
-  if (!values.phoneNumber || values.phoneNumber.trim() === '') {
-    errors.phoneNumber = 'Required';
+  if (!values.phoneNumber) {
+    errors.phoneNumber = 'Champ obligatoire';
+  } else if (!/^(01|06|07)[0-9]{8}$/i.test(values.phoneNumber)) {
+    errors.phoneNumber = 'Numéro de téléphone incorrect';
   }
   return errors;
 };
