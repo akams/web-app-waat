bc0ca21a88ea11a9bca207fa9bb7410ccc932d53
akams <e.ndjawe@gmail.com>
2020-10-28 22:00:57 +0100

    feat: add logout function

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
@@ -12,67 +10,66 @@ import {
   Container,
   Media,
 } from 'reactstrap';
+import { compose } from 'recompose';
+import { withFirebase } from '../../context/firebase';
 import './styles.scss';
 
-class AdminNavbar extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar className="navbar-top navbar-dark bg-gradient-green" expand="md" id="navbar-main">
-          <Container fluid>
-            <Link className="h4 mb-0 text-white text-uppercase d-none d-md-inline-block" to="/">
-              {this.props.brandText}
-            </Link>
-            <Nav className="align-items-center d-none d-md-flex" navbar>
-              <UncontrolledDropdown nav>
-                <DropdownToggle className="pr-0" nav>
-                  <Media className="align-items-center">
-                    <span className="avatar avatar-sm rounded-circle">
-                      <img
-                        alt="..."
-                        src="../assets/img/theme/team-4-800x800.jpg"
-                      />
-                    </span>
-                    <Media className="ml-2 d-none d-lg-block">
-                      <span className="mb-0 text-sm font-weight-bold">
-                        Lyes Cheurfa
-                      </span>
-                    </Media>
+function AdminNavbar(props) {
+  const { brandText, firebase } = props;
+  const logout = async () => {
+    await firebase.logout();
+  };
+  return (
+    <>
+      <Navbar className="navbar-top navbar-dark bg-gradient-green" expand="md" id="navbar-main">
+        <Container fluid>
+          <Link className="h4 mb-0 text-white text-uppercase d-none d-md-inline-block" to="/">
+            {brandText}
+          </Link>
+          <Nav className="align-items-center d-none d-md-flex" navbar>
+            <UncontrolledDropdown nav>
+              <DropdownToggle className="pr-0" nav>
+                <Media className="align-items-center">
+                  <span className="avatar avatar-sm rounded-circle">
+                    <img alt="..." src="../assets/img/theme/team-4-800x800.jpg" />
+                  </span>
+                  <Media className="ml-2 d-none d-lg-block">
+                    <span className="mb-0 text-sm font-weight-bold">Lyes Cheurfa</span>
                   </Media>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem className="noti-title" header tag="div">
-                    <h6 className="text-overflow m-0">Welcome!</h6>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-single-02" />
-                    <span>My profile</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Settings</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-calendar-grid-58" />
-                    <span>Activity</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-support-16" />
-                    <span>Support</span>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem href="">
-                    <i className="ni ni-user-run" />
-                    <span>Logout</span>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
-    );
-  }
+                </Media>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-menu-arrow" right>
+                <DropdownItem className="noti-title" header tag="div">
+                  <h6 className="text-overflow m-0">Welcome!</h6>
+                </DropdownItem>
+                <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <i className="ni ni-single-02" />
+                  <span>My profile</span>
+                </DropdownItem>
+                <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <i className="ni ni-settings-gear-65" />
+                  <span>Settings</span>
+                </DropdownItem>
+                <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <i className="ni ni-calendar-grid-58" />
+                  <span>Activity</span>
+                </DropdownItem>
+                <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <i className="ni ni-support-16" />
+                  <span>Support</span>
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem onClick={logout}>
+                  <i className="ni ni-user-run" />
+                  <span>Logout</span>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
-export default AdminNavbar;
+export default compose(withFirebase)(AdminNavbar);
