bd8652b4120a917b500b9141d12318b6f04719b8
akams <e.ndjawe@gmail.com>
2020-10-31 02:18:11 +0100

    fix: sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,28 +1,65 @@
-/* eslint-disable react/no-unused-state */
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { NavLink as NavLinkRRD, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-import { Collapse, NavbarBrand, Navbar, NavItem, NavLink, Nav, Container, Row, Col } from 'reactstrap';
+import { Card, CardBody, Collapse, NavbarBrand, Navbar, NavItem, NavLink, Nav, Container, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 import { checkAuthorizationWithRoutes } from '../../services/auth.service';
 import { SideMenuUl as SideMenuUlComponent, SideMenuItemLi as SideMenuItemLiComponent } from './styles';
 
-function Sidebar(props) {
-  const { user, routes } = props;
-  const [collapseOpen, setToggleCollapse] = useState(false);
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
 
-  const closeCollapse = () => {
-    setToggleCollapse(false);
-  };
+    this.state = {
+      collapseOpen: false,
+      activeIndex: null,
+    };
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+
+  setToggleCollapse(collapseOpen) {
+    this.setState({ collapseOpen });
+  }
+
+  toggleClass(index) {
+    const { activeIndex } = this.state;
+    console.log({ activeIndex, index });
+    this.setState({
+      activeIndex: activeIndex === index ? null : index,
+    });
+  }
+
+  moreLess(index) {
+    const { activeIndex } = this.state;
+    if (activeIndex === index) {
+      return (
+        <span>
+          <FaAngleUp />
+        </span>
+      );
+    }
+    return (
+      <span>
+        <FaAngleDown />
+      </span>
+    );
+  }
+
+  closeCollapse() {
+    this.setToggleCollapse(false);
+  }
 
   /**
    * Create links aside navigation
    * @param {*} roads
    */
-  const createLinks = (roads) => {
+  createLinks(roads) {
     const {
       location: { pathname },
-    } = props;
+      user,
+    } = this.props;
+    // const { activeIndex } = this.state;
     console.log({ user });
     return roads.map((prop, key) => {
       if (checkAuthorizationWithRoutes(user, pathname)) {
@@ -31,93 +68,102 @@ function Sidebar(props) {
             <NavLink
               to={prop.path}
               tag={NavLinkRRD}
-              onClick={closeCollapse}
+              onClick={() => {
+                this.closeCollapse();
+                // this.toggleClass(key);
+              }}
               activeClassName={prop.path === pathname ? 'active' : ''}
             >
               <i className={`${prop.path === pathname ? `${prop.icon} text-primary` : `${prop.icon}`}`} />
               {prop.name}
             </NavLink>
-            {/* <div style={{ display: !collapseOpen ? 'block' : 'none' }}>
-          <Sidebar.Ul>
-            <Sidebar.Li>Page 1</Sidebar.Li>
-            <Sidebar.Li>Page 2</Sidebar.Li>
-          </Sidebar.Ul>
-        </div> */}
+            {/* 
+            <Collapse isOpen={activeIndex === key}>
+              <Sidebar.Ul>
+                <Sidebar.Li className="mt-1 mb-1">Page 1</Sidebar.Li>
+                <Sidebar.Li>Page 2</Sidebar.Li>
+              </Sidebar.Ul>
+            </Collapse>
+            */}
           </NavItem>
         );
       }
       return null;
     });
-  };
+  }
 
-  return (
-    <Navbar className="navbar-vertical fixed-left navbar-light bg-white" expand="md" id="sidenav-main">
-      <Container fluid>
-        {/* Toggler */}
-        <button className="navbar-toggler" type="button" onClick={setToggleCollapse}>
-          <span className="navbar-toggler-icon" />
-        </button>
-        {/* Brand */}
-        <NavbarBrand className="pt-0">
-          <img
-            title="La Recharge Responsable"
-            alt="La Recharge Responsable"
-            className="navbar-brand-img"
-            src="https://waat.fr/wp-content/uploads/2018/05/logo-waat-v10-rvb.svg"
-          />
-        </NavbarBrand>
-        {/* Collapse */}
-        <Collapse navbar isOpen={collapseOpen}>
-          {/* Collapse header */}
-          <div className="navbar-collapse-header d-md-none">
-            <Row>
-              <Col className="collapse-brand" xs="6">
-                <img
-                  title="La Recharge Responsable"
-                  alt="La Recharge Responsable"
-                  className="navbar-brand-img"
-                  src="https://waat.fr/wp-content/uploads/2018/05/logo-waat-v10-rvb.svg"
-                />
-              </Col>
-              <Col className="collapse-close" xs="6">
-                <button className="navbar-toggler" type="button" onClick={closeCollapse}>
-                  <span />
-                  <span />
-                </button>
-              </Col>
-            </Row>
-          </div>
-          {/* Navigation */}
-          <Nav navbar>{createLinks(routes)}</Nav>
-          {/* Divider */}
-          <hr className="my-3" />
-          {/* Heading */}
-          <h6 className="navbar-heading text-muted">Documentation</h6>
-          {/* Navigation */}
-          <Nav className="mb-md-3" navbar>
-            <NavItem>
-              <NavLink href="#">
-                <i className="ni ni-spaceship" />
-                Getting started
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">
-                <i className="ni ni-palette" />
-                Foundation
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">
-                <i className="ni ni-ui-04" />
-                Components
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Container>
-    </Navbar>
-  );
+  render() {
+    const { user, routes } = this.props;
+    const { collapseOpen } = this.state;
+    return (
+      <Navbar className="navbar-vertical fixed-left navbar-light bg-white" expand="md" id="sidenav-main">
+        <Container fluid>
+          {/* Toggler */}
+          <button className="navbar-toggler" type="button" onClick={() => this.setToggleCollapse(!collapseOpen)}>
+            <span className="navbar-toggler-icon" />
+          </button>
+          {/* Brand */}
+          <NavbarBrand className="pt-0">
+            <img
+              title="La Recharge Responsable"
+              alt="La Recharge Responsable"
+              className="navbar-brand-img"
+              src="https://waat.fr/wp-content/uploads/2018/05/logo-waat-v10-rvb.svg"
+            />
+          </NavbarBrand>
+          {/* Collapse */}
+          <Collapse navbar isOpen={collapseOpen}>
+            {/* Collapse header */}
+            <div className="navbar-collapse-header d-md-none">
+              <Row>
+                <Col className="collapse-brand" xs="6">
+                  <img
+                    title="La Recharge Responsable"
+                    alt="La Recharge Responsable"
+                    className="navbar-brand-img"
+                    src="https://waat.fr/wp-content/uploads/2018/05/logo-waat-v10-rvb.svg"
+                  />
+                </Col>
+                <Col className="collapse-close" xs="6">
+                  <button className="navbar-toggler" type="button" onClick={this.closeCollapse}>
+                    <span />
+                    <span />
+                  </button>
+                </Col>
+              </Row>
+            </div>
+            {/* Navigation */}
+            <Nav navbar>{this.createLinks(routes)}</Nav>
+            {/* Divider */}
+            <hr className="my-3" />
+            {/* Heading */}
+            <h6 className="navbar-heading text-muted">Documentation</h6>
+            {/* Navigation */}
+            <Nav className="mb-md-3" navbar>
+              <NavItem>
+                <NavLink href="#">
+                  <i className="ni ni-spaceship" />
+                  Getting started
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">
+                  <i className="ni ni-palette" />
+                  Foundation
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">
+                  <i className="ni ni-ui-04" />
+                  Components
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 Sidebar.Ul = function SideMenuUl({ children, ...restProps }) {
