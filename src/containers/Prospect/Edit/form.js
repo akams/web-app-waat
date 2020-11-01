2cec2be8ca5047acb64b457ad1a03224729c0719
akams <e.ndjawe@gmail.com>
2020-11-01 03:06:48 +0100

    feat: initialize subform for abonnement

--- src/containers/Prospect/Edit/form.js
+++ src/containers/Prospect/Edit/form.js
@@ -8,7 +8,7 @@ import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 import validate from './validation';
 import { createInitFormData } from '../../../redux/form/helpers';
 import { subForm as mainInfoSubForm, renderMainInfo } from './renderForm/mainInfo/renderMain';
-// import { subForm as abonnementSubForm, renderAbonnement } from './renderForm/abonnement/renderAbonnement';
+import { subForm as abonnementSubForm, renderAbonnement } from './renderForm/abonnement/renderAbonnement';
 // import { subForm as infoPriceSubForm, renderInfoPrice } from './renderForm/infoPrice/renderInfoPrice';
 // import { subForm as keyDateSubForm, renderKeyDate } from './renderForm/keyDate/renderKeyDate';
 
@@ -21,44 +21,73 @@ const changeFormActionCreator = (...rest) => change(formName, ...rest);
 
 function EditForm(props) {
   const { originalOnSubmit, handleSubmit } = props;
-  const [collapse, setCollapse] = useState(false);
-  const toggle = () => setCollapse(!collapse);
-  const componentFaAngle = collapse ? <FaAngleUp onClick={toggle} /> : <FaAngleDown onClick={toggle} />;
+  const [collapseIT, setCollapseIT] = useState(false);
+  const [collapseAbo, setCollapseAbo] = useState(true);
+  // const [collapseAbo, setCollapseAbo] = useState(false);
+  // const [collapseAbo, setCollapseAbo] = useState(false);
+
+  const toggleIT = () => setCollapseIT(!collapseIT);
+  const toggleAbo = () => setCollapseAbo(!collapseAbo);
+  // const toggleAbo = () => setCollapseAbo(!collapseAbo);
+  // const toggleAbo = () => setCollapseAbo(!collapseAbo);
+
+  const componentFaAngleIT = collapseIT ? <FaAngleUp onClick={toggleIT} /> : <FaAngleDown onClick={toggleIT} />;
+  const componentFaAngleAbo = collapseAbo ? <FaAngleUp onClick={toggleAbo} /> : <FaAngleDown onClick={toggleAbo} />;
+  // const componentFaAngleIT = collapseIT ? <FaAngleUp onClick={toggleIT} /> : <FaAngleDown onClick={toggleIT} />;
+  // const componentFaAngleIT = collapseIT ? <FaAngleUp onClick={toggleIT} /> : <FaAngleDown onClick={toggleIT} />;
   return (
     <CardBody>
       <Form className="form-edit-prospect">
         <div>
           <h6 className="heading-small text-muted mb-4">
-            Information technique <span className="icon-angle-edge">{componentFaAngle}</span>
+            Information utilisateur <span className="icon-angle-edge">{componentFaAngleIT}</span>
           </h6>
-          <Fields
-            names={[
-              `${mainInfoSubForm}.lastname`,
-              `${mainInfoSubForm}.firstname`,
-              `${mainInfoSubForm}.address`,
-              `${mainInfoSubForm}.phoneNumber`,
-              `${mainInfoSubForm}.leadTransmissionDate`,
-              `${mainInfoSubForm}.email`,
-              `${mainInfoSubForm}.datePriseContactTel`,
-              `${mainInfoSubForm}.comments`,
-              `${mainInfoSubForm}.typeHabitation`,
-            ]}
-            component={renderMainInfo}
-            props={{
-              changeFormActionCreator,
-              originalOnSubmit,
-              handleSubmit,
-            }}
-          />
+          <Collapse isOpen={collapseIT}>
+            <Fields
+              names={[
+                `${mainInfoSubForm}.lastname`,
+                `${mainInfoSubForm}.firstname`,
+                `${mainInfoSubForm}.address`,
+                `${mainInfoSubForm}.phoneNumber`,
+                `${mainInfoSubForm}.leadTransmissionDate`,
+                `${mainInfoSubForm}.email`,
+                `${mainInfoSubForm}.datePriseContactTel`,
+                `${mainInfoSubForm}.comments`,
+                `${mainInfoSubForm}.typeHabitation`,
+              ]}
+              component={renderMainInfo}
+              props={{
+                changeFormActionCreator,
+                originalOnSubmit,
+                handleSubmit,
+              }}
+            />
+          </Collapse>
+          <hr className="my-4" />
+          <h6 className="heading-small text-muted mb-4">
+            Information technique <span className="icon-angle-edge">{componentFaAngleAbo}</span>
+          </h6>
+          <Collapse isOpen={collapseAbo}>
+            <Fields
+              names={[
+                `${abonnementSubForm}.typeAbo`,
+                `${abonnementSubForm}.distanceApproximativeCable`,
+                `${abonnementSubForm}.emplacementBorne`,
+                `${abonnementSubForm}.emplacementTableau`,
+                `${abonnementSubForm}.isDispoTableau`,
+                `${abonnementSubForm}.percementARealiser`,
+                `${abonnementSubForm}.plugChargeDacces`,
+                `${abonnementSubForm}.comments`,
+              ]}
+              component={renderAbonnement}
+              props={{
+                changeFormActionCreator,
+                originalOnSubmit,
+                handleSubmit,
+              }}
+            />
+          </Collapse>
         </div>
-        <Collapse isOpen={collapse}>
-          <Card>
-            <CardBody>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim
-              keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-            </CardBody>
-          </Card>
-        </Collapse>
       </Form>
     </CardBody>
   );
--- src/containers/Prospect/Edit/renderForm/abonnement/renderAbonnement.js
+++ src/containers/Prospect/Edit/renderForm/abonnement/renderAbonnement.js
@@ -1,16 +1,133 @@
 import React from 'react';
 import { Row, Col, Button, FormGroup, Form, Input, CardBody } from 'reactstrap';
+import { Field } from 'redux-form';
+import {
+  renderInputLabelGroupField,
+  renderSelectLabelGroupField,
+  renderDatePickerLabelGroupField,
+} from '../../../../../redux/form/renderers';
 
 export const subForm = 'abonnement';
 
-export function renderAbonnement(props) {
+export function renderAbonnement({ changeFormActionCreator, originalOnSubmit, handleSubmit, ...fields }) {
+  const onHandleSubmit = (data) => {
+    originalOnSubmit(data);
+  };
   return (
     <>
-      <h6 className="heading-small text-muted mb-4">Information technique</h6>
       <div className="pl-lg-2">
         <Row>
-          <Col lg="6">test abo</Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="typeAbo"
+              labelTxt="Abonnement (sur compteur / facture elect.)"
+              placeholder="6Kva"
+              type="text"
+              name={`${subForm}.typeAbo`}
+              id="typeAbo"
+              component={renderInputLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="distanceApproximativeCable"
+              labelTxt="Distance de câbles approximative"
+              placeholder="3 mètres"
+              type="text"
+              name={`${subForm}.distanceApproximativeCable`}
+              id="distanceApproximativeCable"
+              component={renderInputLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="emplacementBorne"
+              labelTxt="Emplacement de la borne"
+              type="text"
+              name={`${subForm}.emplacementBorne`}
+              id="emplacementBorne"
+              component={renderInputLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="emplacementTableau"
+              labelTxt="Emplacement tableau"
+              type="text"
+              name={`${subForm}.emplacementTableau`}
+              id="emplacementTableau"
+              component={renderInputLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="isDispoTableau"
+              labelTxt="Place disponible dans le tableau"
+              name={`${subForm}.isDispoTableau`}
+              id="isDispoTableau"
+              options={[
+                { id: 1, label: 'Oui' },
+                { id: 2, label: 'Non' },
+              ]}
+              component={renderSelectLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="percementARealiser"
+              labelTxt="Percement à réaliser"
+              name={`${subForm}.percementARealiser`}
+              id="percementARealiser"
+              options={[
+                { id: 1, label: 'Oui' },
+                { id: 2, label: 'Non' },
+              ]}
+              component={renderSelectLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="plugChargeDacces"
+              labelTxt="Plug & Charge ou controle d'acces ?"
+              name={`${subForm}.plugChargeDacces`}
+              id="plugChargeDacces"
+              options={[
+                { id: 1, label: 'Oui' },
+                { id: 2, label: 'Non' },
+              ]}
+              component={renderSelectLabelGroupField}
+            />
+          </Col>
+          <Col lg="6">
+            <Field
+              labelClass="form-control-label"
+              inputClass="form-control-alternative"
+              labelFor="comments"
+              labelTxt="Commentaire"
+              type="textarea"
+              name={`${subForm}.comments`}
+              id="comments"
+              component={renderInputLabelGroupField}
+            />
+          </Col>
         </Row>
+        <Button color="info" onClick={handleSubmit(onHandleSubmit)} size="md">
+          Enregistrer
+        </Button>
       </div>
     </>
   );
