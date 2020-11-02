17b28ecfee4e243c1c86ffc3967051d9a03297a0
akams <e.ndjawe@gmail.com>
2020-11-02 01:36:32 +0100

    prevent from warning

--- src/containers/Prospect/Edit/index.js
+++ src/containers/Prospect/Edit/index.js
@@ -87,6 +87,7 @@ export default compose(
   withFirebase,
   reduxForm({
     form: formName,
+    asyncBlurFields: [],
   }),
   connect(mapStateToProps, mapDispatchToProps)
 )(ProspectEditContainer);
